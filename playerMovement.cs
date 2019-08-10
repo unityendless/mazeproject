640e81c2fbc3eaaa5030e675c96385371174e25f
ranjotbhangu <50412893+ranjotbhangu@users.noreply.github.com>
2019-08-10 11:44:21 +0530

    endless

---
+++ playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0,0,speed*Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0,0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
